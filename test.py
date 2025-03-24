b7d3683df634b6dcef71b2a6c1e64db75fc74d14
issvishchev812 <issvishchev@gmail.com>
2025-03-24 18:43:52 +0300

    final

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ from flask import Flask, render_template, redirect
 app = Flask(__name__)
 
 
-@app.route('/list_prof/<prof>')
+@app.route('/training/<prof>')
 def q(prof):
     return render_template('index.html', prof=prof)
 
 
 if __name__ == '__main__':
-    app.run(port=8080, host='127.0.0.1')
+    app.run(port=5252, host='127.0.0.1')
